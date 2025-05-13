69f885ac135eb4debd62c789ed837ed3e0aed478
GreenJon902 <37347905+GreenJon902@users.noreply.github.com>
2025-05-13 19:57:42 +0100

    Update originalCode.js

--- originalCode.js
+++ originalCode.js
@@ -1,15 +1,19 @@
 //self invoked function (so it doesn't pollute the global scope)
 (async function () {
-  document.getElementById("app").classList.remove("By1np");  // Remove the "Please use the website's Print button to print this page." message.
+   // Remove the "Please use the website's Print button to print this page." message.
+  document.getElementById("app").classList.remove("By1np"); 
+
+  // Disable the @media print {display: none;} on the tab elements.
   const style = document.createElement("style");
   style.textContent = `
     @media print {
-      Cw81bf, B6413b, Chcfq, Cpk34y {
+      .Cw81bf, .B6413b, .Chcfq, .Cpk34y {
         display: revert;
       }
     }
   `;
   document.head.appendChild(style);
 
-
+  // Bring up the print dialogue
+  print();  
 })();
